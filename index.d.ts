dcccc2ffd514bcb6dd3ee197028982a86f73e04f
Jehú <jehu.sagardoy@gmail.com>
2019-03-26 17:13:33 +0100

    updated name

--- index.d.ts
+++ index.d.ts
@@ -2,6 +2,7 @@
  * Validates an input value as IBAN (International Bank Account Number)
  * @param value Input value to be interpreted as IBAN
  */
-import {FieldValidationResult} from 'lc-form-validation';
+
+import FieldValidationResult from 'lc-form-validation';
 export declare const VALIDATION_TYPE : string;
-export declare function validateIBAN (value: any) : FieldValidationResult;
+export declare function validateIBAN(value: any) : FieldValidationResult;
